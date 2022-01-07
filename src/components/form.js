f1c19bf4ad9c9b7e3f92e2e953edfccf032bf007
Ruta Tuckute <Ruta.Tuckute@mfex.com>
2022-01-07 23:34:21 +0100

    test

--- src/components/form.js
+++ src/components/form.js
@@ -1,14 +1,32 @@
 import * as React from "react"
-import { Card, Container, Row } from 'react-bootstrap'
+import { Card, Container, Row, Form, Button } from 'react-bootstrap'
 
-const Form = () => {
+const ContactForm = () => {
 
     return (
         <Container fluid>
             <Row className="justify-content-md-center">
                 <Card border="secondary" className="data">
                     <Card.Body>
-                    <form name="contacts" method="post" data-netlify="true">
+                    <Form name="contacts" method="post" data-netlify="true">
+                    <input type="hidden" name="form-name" value="contacts"/>
+                    <Form.Group className="mb-3" controlId="formPlaintext">
+                        <Form.Label>Name</Form.Label>
+                            <Form.Control type="text" defaultValue="Name" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                            <Form.Label>Your message</Form.Label>
+                            <Form.Control as="textarea" rows={3} />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+                    {/* <form name="contacts" method="post" data-netlify="true">
                         <input type="hidden" name="form-name" value="contacts"/>
                     <p>
                     <label htmlFor="name">Name</label> <br />
@@ -25,7 +43,7 @@ const Form = () => {
                     <p>
                     <input type="submit" value="Submit" />
                     </p>
-                </form>
+                </form> */}
                 </Card.Body>
             </Card>
         </Row>
@@ -33,4 +51,4 @@ const Form = () => {
     )
 }
 
-export default Form
+export default ContactForm
--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -1,11 +1,11 @@
 import * as React from "react"
 import Layout from "../components/layout"
-import Form from "../components/form"
+import ContactForm from "../components/form"
 
 const Contacts = ({location}) => {
   return (
     <Layout location={location}>
-      <Form/>
+      <ContactForm/>
     </Layout>
   );
 };
