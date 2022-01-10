c47bb83080516865c22781bcb96996b2df542165
Ruta Tuckute <Ruta.Tuckute@mfex.com>
2022-01-10 16:32:40 +0100

    about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,24 +1,25 @@
 import * as React from "react"
 import Layout from "../components/layout"
+import { Container, Row, Col } from 'react-bootstrap'
 
 
 const About = ({ location }) => {
 
   return (
     <Layout location={location}>
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '60vh'
-      }}
-    >
-      <h1>In Brief</h1>
-      
-      <img src="https://ucarecdn.com/92e473be-2fa6-443b-9f58-998156630420/iconII.png" style={{height:"300px"}}></img>
-      <p style={{color:"white"}}>I never felt like writing anything fictional.</p>
-    </div>
+      <Container flex className="bref-container">
+      <Row className="justify-content-md-center">
+        <Col md="auto">
+          <h1 class="bref">In Brief</h1>
+        </Col>
+        <Col md="auto">
+          <img src="https://ucarecdn.com/92e473be-2fa6-443b-9f58-998156630420/iconII.png" style={{height:"300px"}}></img>
+        </Col>
+        <Col md="auto">
+          <p class="bref">I never felt like writing anything fictional.</p>
+        </Col>
+      </Row>
+      </Container>
     </Layout>
 
   );
